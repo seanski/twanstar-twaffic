1a09e15484f056683eb17fd2df847b80e73e165f
Sean Hill <seandhi@gmail.com>
2010-09-12 16:05:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellado::Application.config.secret_token = '31303e85c4a2cd91886926337c9b44087def3113cca7d8253720780a83e0bf08051fa3e86e7f40b802463135c4e5068802d8c1a1d408ed03c84c788eddd5bc51'
